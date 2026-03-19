6c8f10b8934b0544ef8fcd6ef143f4b44fd0bea4
simonlei <simonlei@qq.com>
2026-03-19 15:57:24 +0800

    "chore:add-testing-deps-and-directory-structure"

---
+++ android/app/src/test/java/com/photoframe/SmokeTest.kt
@@ -0,0 +1,11 @@
+package com.photoframe
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+
+class SmokeTest {
+    @Test
+    fun `test infrastructure works`() {
+        assertTrue(true, "JUnit 5 is working")
+    }
+}
